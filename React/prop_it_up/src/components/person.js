4757e08909db5b8bfb9b7b31bc0a92c89640e848
Ben Tsai <benjamintsa@gmail.com>
2020-09-22 03:55:51 -0400

    putting it together assignment

--- React/prop_it_up/src/components/person.js
+++ React/prop_it_up/src/components/person.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
 
 class Person extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            age: props.age
+        };
+    }
+
+    increaseAge = () => {
+        let newAge = parseInt(this.state.age)+1;
+        this.setState({age: newAge});
+    }
+
     render(){
         return <div className = "person">
             <h1>{this.props.lastName}, {this.props.firstName} </h1>
-            <p>Age: {this.props.age}</p>
+            <p>Age: {this.state.age}</p>
             <p>Hair Color: {this.props.hairColor}</p>
+            <button onClick = {this.increaseAge}>Birthday button for {this.props.firstName} {this.props.lastName}</button>
         </div>
     }
+
 }
 
 export default Person;
